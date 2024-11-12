e9ad1fec2abaf052277f220e1d8b9437fd936e06
duda_mafra <129239516+maria211005@users.noreply.github.com>
2024-11-12 12:11:07 +0000

    ARRUMEI AS CONDIÇÕES DE ERRO!!!!!!

--- atividade 1/atividade.py
+++ atividade 1/atividade.py
@@ -2,24 +2,25 @@
 from random import randint
 import time
 
-with open("input8.txt", "r") as f:
-    quant_numeros = f.readline().strip()
-    if type(quant_numeros) != int:
-        print("Erro ao receber variável")
+with open("input9.txt", "r") as f:
+    if f.readline() == ' ':
+        print("Arquivo vazio")
         exit(1)
+    f.seek(0)
+    quant_numeros = f.readline().strip()
     tipo_ord =  f.readline().strip()
     f.close()
         
 print(quant_numeros, tipo_ord)
 dados = []
 if tipo_ord == 'c':
-    for i in range (1, quant_numeros+1):
+    for i in range (1, int(quant_numeros)+1):
         dados.append(i)
 elif tipo_ord == 'd':
-    for i in range (quant_numeros, 0, -1):
+    for i in range (int(quant_numeros), 0, -1):
         dados.append(i)
 elif tipo_ord == 'r':
-    for i in range (quant_numeros):
+    for i in range (int(quant_numeros)):
         dados.append(randint(0, 32000))
 else:
     print("Fora dos padrões: 'c', 'd' ou 'r'\nO programa será finalizado")
