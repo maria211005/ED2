0783368d7124aa55e11a065b0cc6f6b8a55cabba
duda_mafra <129239516+maria211005@users.noreply.github.com>
2024-11-21 21:36:02 -0300

    Co-authored-by: sartori_mi <sartorimi@users.noreply.github.com>

--- entregavel7.py
+++ entregavel7.py
@@ -3,51 +3,39 @@ def AbreArquivo():
     with open("animes.csv", 'r') as f: #da pra tirar da função 1 isso aqui 
         registros = f.readlines()
         if registros == '':
-            print('O arquivo est� vazio\n')
+            print('O arquivo está vazio\n')
             exit(1)
         else:
+            registros.pop(0)
             f.close()
     return registros 
-    
-
+#------------------------------------------------------------------    
+#METODO 1 - ESCRITA DE TAMANHO FIXO
 def escritaTamanhoFixo(registros): #passar o registros pra ca 
-    tamanhos = []
     for linha in range(len(registros)): #lendo as linhas e pegando o tamanho de cada uma 
-        contaBytes = 0
-        while linha.seek(1) != '\n': 
-            contaBytes = contaBytes+1 #contando o tamanho de cada linha
-        tamanhos[linha] = contaBytes
-
-    maiorValor = tamanhos[0]
-
-    for i in range(len(tamanhos)): #acha a linha com maior valor 
-        if(tamanhos[i] > maiorValor):
-            maiorValor = tamanhos[i]
-
-    TamReg = maiorValor
-
+        linha = linha.strip('\n')
+    
+    TamReg = max(registros)
+    
     with open('metodo1.txt', 'w') as output:
         for linha in registros:
             str.replace(",","|")
         
             if len(linha) < TamReg:
-                newStr = linha + '*'*(TamReg - len(linha))
+                newStr = linha + '*'*(TamReg - len(linha)-1) + '\n'
             else:
                 newStr = linha
             
             output.write(newStr)
-
 #--------------------------------------------------------
 #METODO 2 - QTDD FIXA DE CAMPOS 
 def escritaQtdeCampos(registros):
-    #41 campos formam um registro ---> fazer um contador de pipe 
-    #a cada 41 campos, concatenar 
-
     with open('metodo2.txt', 'w') as output:
         for linha in range(len(registros)): #percorrendo o arquivo todo 
+            str.replace(",", "|")
             newStr = linha
-            output.write(newStr[:len(newStr-2)]) 
-#----------------------------------------------------------------
+            output.write(newStr[:len(newStr)-1]) 
+#----------------------------------------------------------
 #METODO 3 - QTTD BYTES
 def escritaQtdeBytes(registros):
     with open('metodo3.txt', 'w') as output:
@@ -56,19 +44,39 @@ def escritaQtdeBytes(registros):
             newStr = linha
             while (linha.seek(1) != '\n'):
                 contadorBytes = contadorBytes + 1 
-            output.write(contadorBytes + newStr[:len(newStr-2)])
-
+            output.write(contadorBytes + newStr[:len(newStr-1)])
 #------------------------------------------------------------
 #METODO 4 - ARQUIVO DE INDEX 
 def escritaArqIndex(registros):
-    contadorBytes = 0
+    somador = 0
     with open('metodo4.txt', 'w') as output:
+        output.write(somador)
         for linha in range(len(registros)):
-                    
-
-
+            linha = linha.strip('\n') #tirando \n de tudo
+            while (linha.seek(1) != ''):
+                somador = somador + 1
+            if linha.seek(0) != None:
+                valorPosicao = somador + 1
+                output.write(valorPosicao + '')
+#-------------------------------------------------------------
+#METODO 5 - DELIMITADORES 
+def escritaDelimitadores(registros):
+    with open('metodo5.txt', 'w') as output:
+        for linha in range(len(registros)):
+            linha = linha.strip('\n')
+            output.write(linha + '#')
+#-------------------------------------------------------------
+#função principal
 if __name__ == "__main__":
     registro = AbreArquivo()
-    escritaTamanhoFixo(registro)
-    escritaQtdeCampos(registro)
-    escritaQtdeBytes(registro)
+    reg1 = registro
+    reg2 = registro
+    reg3 = registro
+    reg4 = registro
+    reg5 = registro
+
+    escritaTamanhoFixo(reg1)
+    escritaQtdeCampos(reg2)
+    escritaQtdeBytes(reg3)
+    escritaArqIndex(reg4)
+    escritaDelimitadores(reg5)
