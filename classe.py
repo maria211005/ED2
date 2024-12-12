ecde098c79c05f1571f85df60a228f69340c46f0
duda_mafra <129239516+maria211005@users.noreply.github.com>
2024-12-12 03:17:21 +0000

    criei um novo pra testar a classe objeto

--- classe.py
+++ classe.py
@@ -21,14 +21,39 @@ class Heroi:
 
 
 if __name__ == '__main__':
-    with open('saida2.txt', 'r') as entrada:
+    with open('input01.txt', 'r') as entrada:
         linha = entrada.readline()
+        linha2 = entrada.readline()
         registro = entrada.readlines()
-        key = []
+        arq = []
         for linha in registro:
             linha_sep = linha.split(sep='|')
             resultado = Heroi(linha_sep)
-            key.append(resultado.key)
+            arq.append(resultado.key)
+            arq.append(resultado.Name)
+            arq.append(resultado.Alignment)
+            arq.append(resultado.Gender)
+            arq.append(resultado.EyeColor)
+            arq.append(resultado.Race)
+            arq.append(resultado.HairColor)
+            arq.append(resultado.Publisher)
+            arq.append(resultado.SkinColor)
+            arq.append(resultado.Height)
+            arq.append(resultado.Weight)
+            arq.append(resultado.Intelligence)
+            arq.append(resultado.Strength)
+            arq.append(resultado.Speed)
+            arq.append(resultado.Durability)
+            arq.append(resultado.Power)
+            arq.append(resultado.Combat)
+            arq.append(resultado.Total)
+    
+    print(arq)
 
-        print(key)
-            
+    with open('saida.txt', 'w') as output:
+        output.write(linha2)
+        for i in range(len(arq)):
+            if '\n' in arq[i]:
+                output.write(arq[i])
+            else:
+                output.write(arq[i] + "|")
