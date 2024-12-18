6a3f9c2745ce09931fc72852122ed529da1846c3
sartori_mi <145930163+sartorimi@users.noreply.github.com>
2024-12-17 21:04:05 -0300

    so escrevi o pseudocod de cada

--- entregavel10.py
+++ entregavel10.py
@@ -1,1 +1,21 @@
 #só abri o arquivo pra dizer q fiz algo agora KKKKKKKKKKKKKKKKKKKKKKKKKKK
+
+#PESQUISA
+#1) Procurar a chave na tabela de índices
+#--> busca binária
+#--> se achou ---> (chave, RRN)
+#--> calcular offset
+#--> leitura usando o RRN
+#--> retorna registro para o usuário
+
+#INSERÇÃO
+#1) gerar una chave para o novo registro
+#2) realizar uma pesquisa (chamar a função anterior)
+#--> verifica se o novo registro ja existe (tem que retornar vazio)
+#3) leitura do cabeçalho (last)
+#--> se for last == -1:
+#    RRN + 1
+#    adicionar o registro no fim do arquivo 
+#    adicionar na tabela de índices (chave, RRN)
+#    ordenar a tabela de índices
+
