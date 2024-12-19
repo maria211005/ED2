271ab841d84b8d183aa0f7c995e9700ebec3739e
duda_mafra <129239516+maria211005@users.noreply.github.com>
2024-12-19 12:11:58 +0000

    comecei algumas coisas

--- entregavel10.py
+++ entregavel10.py
@@ -1,3 +1,13 @@
+import sys
+
+entrada = ''
+#caso não tenha a quantidade de argumentos válida para execução do código
+if(len(sys.argv) != 3):
+    print("Quantidade de argumentos inválida. Tente novamente")
+    exit(1)
+else:
+    entrada = sys.argv[1]
+
 #PESQUISA
 #1) Procurar a chave na tabela de índices
 #--> busca binária
@@ -18,8 +28,8 @@
 #    ordenar a tabela de índices
 
 def abreArquivo():
-    with open("animes.txt", "r") as file:
-        header = file.readline()
+    with open(entrada, "r") as file:
+        file.seek(len(file.readline()))
         registros = file.readlines()
 
         tupla = []
@@ -28,11 +38,8 @@ def abreArquivo():
             linha_sep = linha.split(",")
             chave = linha_sep[0]
             tupla.append(f"{chave}, {offset}")
-            tamLinha = len(linha)
-            offset += tamLinha
-    return tupla 
-
-abreArquivo()
+            offset += 1
+    return tupla, registros
 
 def ParticionaTupla(tupla, inicio, fim):
     esquerda = inicio 
@@ -43,12 +50,15 @@ def ParticionaTupla(tupla, inicio, fim):
         while(tupla[esquerda] <= pivo and esquerda < fim):
             esquerda = esquerda + 1
 
-    while(direita > esquerda):
-        while(tupla[direita] >= pivo and direita > inicio):
+        while(tupla[direita] > pivo and direita > inicio):
             direita = direita - 1 
 
-    if(esquerda < direita):
-        tupla[esquerda], tupla[direita] = tupla[direita], tupla[esquerda] 
+        if(esquerda < direita):
+            tupla[esquerda], tupla[direita] = tupla[direita], tupla[esquerda]
+    
+    tupla[direita], tupla[inicio] = tupla[inicio], tupla[direita]
+
+    return direita
 
 def OrdenaTupla(tupla, inicio, fim):
     if(inicio < fim):
@@ -56,17 +66,34 @@ def OrdenaTupla(tupla, inicio, fim):
         OrdenaTupla(tupla, inicio, pivo-1)
         OrdenaTupla(tupla, pivo+1, fim)
 
+def escreveArquivoIndices(tupla):
+    saida = "arqIndices.txt"
+    with open(saida, "w") as indices:
+        for i in range(len(tupla)):
+            tupla[i] = tupla[i].split(",")
+            tupla[i][1] = tupla[i][1].strip(" ")
+            indices.write(tupla[i][0] + "|" + tupla[i][1] + "\n")
+        
+    return saida
+
 def BuscaBinaria(tupla, chave, reg):
     inicio = 0
     fim = len(tupla) - 1
     while(inicio <= fim):
-        meio = (inicio + fim) / 2 
-        reg = tupla.readline(meio)
-        #if chave_reg == chave: 
-        #   return true
+        meio = (inicio + fim) // 2 
+        chave_reg = tupla[meio]
+        if chave_reg == chave: 
+           return True
 
         #if chave_reg < chave:
         #   fim = meio - 1 
 
         #else
         #   inicio = meio + 1 
+
+if __name__ == '__main__':
+    tupla, regs = abreArquivo()
+    OrdenaTupla(tupla, 0, len(tupla) -1)
+    arqIndice = escreveArquivoIndices(tupla)
+
+    BuscaBinaria(tupla, "BLEACH", regs)    
