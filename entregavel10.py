28a6f3174c87699b5e9f89e778bd0cc231b1ea45
duda_mafra <129239516+maria211005@users.noreply.github.com>
2024-12-20 15:00:56 +0000

    TA QUASE PRONTO!!!!!

--- entregavel10.py
+++ entregavel10.py
@@ -9,8 +9,7 @@ if(len(sys.argv) != 3):
     exit(1)
 else:
     entrada = sys.argv[1]
-
-entrada2 = "animes2.txt"
+    entrada2 = sys.argv[2]
 #INSERÇÃO
 #1) gerar una chave para o novo registro
 #2) realizar uma pesquisa (chamar a função anterior)
@@ -48,15 +47,19 @@ def abreArquivo():
     with open(entrada2, "w") as output:
         output.write(header)
         for linha in registros:
-            linha = linha.strip("\n")
-            linha = linha.replace(",","|")
-            if len(linha) < maxRegistro:            #verifica o tamanho da linha pra inserir caracteres especiais ou não
-                dif = maxRegistro - len(linha) -1
-                linha = linha + '*' * dif + '\n'
-            else:                                   #caso for a maior linha
-                linha = linha + '\n'
+            linha = organizaLinha(linha, maxRegistro)
             output.write(linha)
     return tupla, registros, maxRegistro
+
+def organizaLinha(linha, maxRegistro):
+    linha = linha.strip("\n")
+    linha = linha.replace(",","|")
+    if len(linha) < maxRegistro:            #verifica o tamanho da linha pra inserir caracteres especiais ou não
+        dif = maxRegistro - len(linha) -1
+        linha = linha + '*' * dif + '\n'
+    else:                                   #caso for a maior linha
+        linha = linha + '\n'
+    return linha
 
 def ParticionaTupla(tupla, inicio, fim):
     esquerda = inicio 
@@ -102,47 +105,60 @@ def BuscaBinaria(tupla, chave):
            inicio = meio + 1
     
     if inicio > fim:
-        return "O registro não foi encontrado\n"
+        return -1
     
 def buscaRegistro(arqEntrada, maxRegistro, chave):
     rrn = int(chave[1])
 
     with open(arqEntrada, 'r') as file:
         file.seek(len(file.readline()) + rrn*maxRegistro)
-        print("Registro Encontrado\n" + file.readline())
+        print(file.readline())
+        #print("Registro Encontrado\n" + file.readline())
 
 def pesquisa_de_registro(tupla, tamRegs, anime, origem):
     chaveEncontrada = BuscaBinaria(tupla, anime)
-    if chaveEncontrada == 'O registro não foi encontrado\n':
+    if chaveEncontrada == -1:
         return chaveEncontrada
     else:
         if origem == 0:
             buscaRegistro(entrada2, tamRegs, chaveEncontrada)
         else:
-            return "O registro já existe\n"
+            return 1
 #-----------------------------------------------------------------------------
 #funções de inserção
 #-----------------------------------------------------------------------------
 def insercao_de_registro(tupla, tamRegs):
     novoRegistro = str(input("Qual o novo registro que deseja inserir:"))
-    novoRegistro = novoRegistro.split("|")
-    chave = pesquisa_de_registro(tupla, tamRegs, novoRegistro[0], 1)
-    print(chave)
+    novoRegistro_sep = novoRegistro.split(",")
+    verificaRegistro = pesquisa_de_registro(tupla, tamRegs, novoRegistro_sep[0], 1)
+    if verificaRegistro == 1:
+        print("O registro já existe\n")
+    else:
+        novachave = novoRegistro_sep[0]
+        rrn = len(tupla)
+        tupla.append(f"{novachave}|{rrn}")
+        OrdenaTupla(tupla, 0, len(tupla)-1)
+
+        with open(entrada, 'a') as output:
+            output.write("\n"+novoRegistro)
+        with open(entrada2, 'a') as output:
+
+            novoRegistro = organizaLinha(novoRegistro, tamRegs)
+            output.write("\n"+novoRegistro)
 #-----------------------------------------------------------------------------
 #função principal
 #-----------------------------------------------------------------------------
 if __name__ == '__main__':
     tupla, regs, tamRegs = abreArquivo()
     OrdenaTupla(tupla, 0, len(tupla) -1)
-
     menu = 1
     while menu != 0:
         menu = int(input("O que deseja fazer:\n1- Pesquisar\n2- Inserir\n0- Sair\n"))
         if menu == 1:
             anime = str(input("Qual anime deseja buscar: "))
             chave = pesquisa_de_registro(tupla, tamRegs, anime, 0)
-            if chave == 'O registro não foi encontrado\n':
-                print(chave)
+            if chave == -1:
+                print('O registro não foi encontrado\n')
         if menu == 2:
             insercao_de_registro(tupla, tamRegs)
         if menu == 0:
