6b0cb69313c14297fefc90a2f52cb5b8f5eb4a17
duda_mafra <129239516+maria211005@users.noreply.github.com>
2024-12-20 12:48:40 +0000

    coloquei a condição de se o registro já existe na inserção

--- entregavel10.py
+++ entregavel10.py
@@ -1,5 +1,7 @@
 import sys
-
+#-----------------------------------------------------------------------------
+#função sys.argv
+#-----------------------------------------------------------------------------
 entrada = ''
 #caso não tenha a quantidade de argumentos válida para execução do código
 if(len(sys.argv) != 3):
@@ -8,28 +10,24 @@ if(len(sys.argv) != 3):
 else:
     entrada = sys.argv[1]
 
-#PESQUISA
-#1) Procurar a chave na tabela de índices
-#--> busca binária
-#--> se achou ---> (chave, RRN)
-#--> calcular offset
-#--> leitura usando o RRN
-#--> retorna registro para o usuário
-
+entrada2 = "animes2.txt"
 #INSERÇÃO
 #1) gerar una chave para o novo registro
 #2) realizar uma pesquisa (chamar a função anterior)
 #--> verifica se o novo registro ja existe (tem que retornar vazio)
 #3) leitura do cabeçalho (last)
-#--> se for last == -1:
+#--> se for last == -1:         (amiga, o professor falou que não precisa fazer de reuso)
 #    RRN + 1
 #    adicionar o registro no fim do arquivo 
 #    adicionar na tabela de índices (chave, RRN)
 #    ordenar a tabela de índices
 
+#-----------------------------------------------------------------------------
+#funções de abertura e organização
+#-----------------------------------------------------------------------------
 def abreArquivo():
     with open(entrada, "r+") as file:
-        file.seek(len(file.readline()))
+        header = file.readline()
         registros = file.readlines()
 
         tupla = []
@@ -47,16 +45,17 @@ def abreArquivo():
             if tam[i] > maxRegistro:
                 maxRegistro = tam[i]
 
-        file.seek(0)
-        file.seek(len(file.readline()))
+    with open(entrada2, "w") as output:
+        output.write(header)
         for linha in registros:
             linha = linha.strip("\n")
+            linha = linha.replace(",","|")
             if len(linha) < maxRegistro:            #verifica o tamanho da linha pra inserir caracteres especiais ou não
                 dif = maxRegistro - len(linha) -1
                 linha = linha + '*' * dif + '\n'
             else:                                   #caso for a maior linha
                 linha = linha + '\n'
-            file.write(linha)
+            output.write(linha)
     return tupla, registros, maxRegistro
 
 def ParticionaTupla(tupla, inicio, fim):
@@ -83,7 +82,9 @@ def OrdenaTupla(tupla, inicio, fim):
         pivo = ParticionaTupla(tupla, inicio, fim) 
         OrdenaTupla(tupla, inicio, pivo-1)
         OrdenaTupla(tupla, pivo+1, fim)
-
+#-----------------------------------------------------------------------------
+#funções de pesquisa
+#-----------------------------------------------------------------------------
 def BuscaBinaria(tupla, chave):
     inicio = 0
     fim = len(tupla) - 1
@@ -109,7 +110,27 @@ def buscaRegistro(arqEntrada, maxRegistro, chave):
     with open(arqEntrada, 'r') as file:
         file.seek(len(file.readline()) + rrn*maxRegistro)
         print("Registro Encontrado\n" + file.readline())
-    
+
+def pesquisa_de_registro(tupla, tamRegs, anime, origem):
+    chaveEncontrada = BuscaBinaria(tupla, anime)
+    if chaveEncontrada == 'O registro não foi encontrado\n':
+        return chaveEncontrada
+    else:
+        if origem == 0:
+            buscaRegistro(entrada2, tamRegs, chaveEncontrada)
+        else:
+            return "O registro já existe\n"
+#-----------------------------------------------------------------------------
+#funções de inserção
+#-----------------------------------------------------------------------------
+def insercao_de_registro(tupla, tamRegs):
+    novoRegistro = str(input("Qual o novo registro que deseja inserir:"))
+    novoRegistro = novoRegistro.split("|")
+    chave = pesquisa_de_registro(tupla, tamRegs, novoRegistro[0], 1)
+    print(chave)
+#-----------------------------------------------------------------------------
+#função principal
+#-----------------------------------------------------------------------------
 if __name__ == '__main__':
     tupla, regs, tamRegs = abreArquivo()
     OrdenaTupla(tupla, 0, len(tupla) -1)
@@ -119,13 +140,11 @@ if __name__ == '__main__':
         menu = int(input("O que deseja fazer:\n1- Pesquisar\n2- Inserir\n0- Sair\n"))
         if menu == 1:
             anime = str(input("Qual anime deseja buscar: "))
-            chaveEncontrada = BuscaBinaria(tupla, anime)
-            if chaveEncontrada == 'O registro não foi encontrado\n':
-                print(chaveEncontrada)
-            else:
-                buscaRegistro(entrada, tamRegs, chaveEncontrada)
-        #if menu == 2:
-            #inserir registro
+            chave = pesquisa_de_registro(tupla, tamRegs, anime, 0)
+            if chave == 'O registro não foi encontrado\n':
+                print(chave)
+        if menu == 2:
+            insercao_de_registro(tupla, tamRegs)
         if menu == 0:
             print("Programa finalizado")
             exit(0)
