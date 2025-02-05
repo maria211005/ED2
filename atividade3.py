ec375729e7a320a22dd05d64346cd674aad7b32c
duda_mafra <129239516+maria211005@users.noreply.github.com>
2025-02-05 18:45:45 +0000

    comecei algumas coisinhas

--- atividade3.py
+++ atividade3.py
@@ -1,1 +1,20 @@
 #atividade de indice secundário
+with open("tracks_menor.csv", 'r') as arq:
+    header = arq.readline()
+    arquivo = arq.readlines()
+
+    print(header)
+    tam = []
+    maxRegistro = 0
+    for i in range(len(arquivo)):
+        arquivo[i] = arquivo[i].strip('\n')
+        tam.append(len(arquivo[i]))
+
+        if tam[i] > maxRegistro:
+            maxRegistro = tam[i]
+    
+    for i in range (len(arquivo)):
+        arquivo[i] = arquivo[i].replace(",","|")
+        if len(arquivo[i]) < maxRegistro:
+            arquivo[i] = arquivo[i] + "*"*(maxRegistro-len(arquivo[i]))
+            print(arquivo[i])
