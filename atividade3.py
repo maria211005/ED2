edf04b1cb3c1bdc27a29c73566e4edf9d9c77983
duda_mafra <129239516+maria211005@users.noreply.github.com>
2025-02-07 18:40:09 +0000

    comecei algo

--- atividade3.py
+++ atividade3.py
@@ -1,20 +1,44 @@
-#atividade de indice secundário
-with open("tracks_menor.csv", 'r') as arq:
+import sys
+
+entrada = ''
+saida = ''
+#caso não tenha a quantidade de argumentos válida para execução do código
+if(len(sys.argv) != 4):
+    print("Quantidade de argumentos inválida. Tente novamente")
+    exit(1)
+else:
+    entrada = sys.argv[1]
+    consulta = sys.argv[2]
+    saida = sys.argv[3]
+'''
+with open(entrada, 'r') as arq:
     header = arq.readline()
     arquivo = arq.readlines()
 
-    print(header)
     tam = []
     maxRegistro = 0
     for i in range(len(arquivo)):
         arquivo[i] = arquivo[i].strip('\n')
+        arquivo[i] = arquivo[i].replace(",","|")
+
         tam.append(len(arquivo[i]))
 
         if tam[i] > maxRegistro:
             maxRegistro = tam[i]
-    
+'''
+with open(consulta, 'r') as query:
+    condicao = query.readline().strip("\n")
+    valor = query.readline()
+
+    if '&' in condicao or '||' in condicao:
+        print("consulta booleana")
+    else:
+        print("consulta simples")
+'''
+with open(saida, 'w') as output:
+    output.write(header)
     for i in range (len(arquivo)):
-        arquivo[i] = arquivo[i].replace(",","|")
         if len(arquivo[i]) < maxRegistro:
-            arquivo[i] = arquivo[i] + "*"*(maxRegistro-len(arquivo[i]))
-            print(arquivo[i])
+            arquivo[i] = arquivo[i] + "|" + "*"*(maxRegistro-len(arquivo[i])) + "\n"
+        output.write(arquivo[i])
+'''
