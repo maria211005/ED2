48110646d46e4f17123ac7b3bbf07ecc42067d8b
duda_mafra <129239516+maria211005@users.noreply.github.com>
2025-02-12 19:31:21 +0000

    avanço

--- atividade3.py
+++ atividade3.py
@@ -10,37 +10,60 @@ else:
     entrada = sys.argv[1]
     consulta = sys.argv[2]
     saida = sys.argv[3]
+#-----------------------------------------------------------------------------------
+#recebe o arquivo de entrada com todos os registros e organiza-os
+def recebeRegistro():
+    with open(entrada, 'r') as arq:
+        header = arq.readline().split(',')
+        arquivo = arq.readlines()
 
-with open(entrada, 'r') as arq:
-    header = arq.readline()
-    arquivo = arq.readlines()
+        #organizando o arquivo para alinhar os registros para procura
+        tam = []
+        maxRegistro = 0
+        for i in range(len(arquivo)):
+            arquivo[i] = arquivo[i].strip('\n')
+            arquivo[i] = arquivo[i].replace(",","|")
 
-    tam = []
-    maxRegistro = 0
-    for i in range(len(arquivo)):
-        arquivo[i] = arquivo[i].strip('\n')
-        arquivo[i] = arquivo[i].replace(",","|")
+            tam.append(len(arquivo[i]))
 
-        tam.append(len(arquivo[i]))
+            if tam[i] > maxRegistro:
+                maxRegistro = tam[i]
 
-        if tam[i] > maxRegistro:
-            maxRegistro = tam[i]
+        for i in range (len(arquivo)):
+            if len(arquivo[i]) < maxRegistro:
+                arquivo[i] = arquivo[i] + "|" + "*"*(maxRegistro-len(arquivo[i])-2) + "\n"
+            else:
+                arquivo[i] = arquivo[i] + "\n"
+        
+    return arquivo, header
+#----------------------------------------------------------------------------------------
+#recebe o arquivo com as condições de consulta e o registro que deseja procurar
+def consultaRegistro(arquivo, cabecalho):
+    with open(consulta, 'r') as query:
+        #name, album, artists, track_number, disc_number, explicit, key, mode, year
+        condicao = query.readline().strip("\n")
+        valor = query.readline()
 
-    for i in range (len(arquivo)):
-        if len(arquivo[i]) < maxRegistro:
-            arquivo[i] = arquivo[i] + "|" + "*"*(maxRegistro-len(arquivo[i])-2) + "\n"
+        if '&' in condicao:
+            print("consulta booleana com &")
+        if '||' in condicao:
+            print("consulta booleana com ||")
         else:
-            arquivo[i] = arquivo[i] + "\n"
-
-with open(consulta, 'r') as query:
-    condicao = query.readline().strip("\n")
-    valor = query.readline()
-
-    if '&' in condicao or '||' in condicao:
-        print("consulta booleana")
-    else:
-        print("consulta simples")
-
+            for i in range(len(cabecalho)):
+                if cabecalho[i] == condicao:
+                    coluna = i
+            #aqui não ta dando certo 
+            for i in range(len(arquivo)):
+                arquivo[i] = arquivo[i].split("|")
+                print(arquivo[i][coluna])
+#---------------------------------------------------------------------------------------
+#arquivo de saída com os registros pesquisados
+'''
 with open(saida, 'w') as output:
     for i in range (len(arquivo)):
         output.write(arquivo[i])
+'''
+
+if __name__ == '__main__':
+    arqRegistros, cabecalho = recebeRegistro()
+    consultaRegistro(arqRegistros, cabecalho)
