fdb691d9b348d074383d8fa094aa6bfc8d68aa0c
duda_mafra <129239516+maria211005@users.noreply.github.com>
2025-02-16 17:13:23 +0000

    acredito que tá pronto, ou só falta uns toques finais

--- atividade3.py
+++ atividade3.py
@@ -1,13 +1,21 @@
-import sys, csv
+import sys, csv, os
 
 #caso não tenha a quantidade de argumentos válida para execução do código
 if(len(sys.argv) != 4):
-    print("Quantidade de argumentos inválida. Tente novamente")
+    if(len(sys.argv) < 4):
+        print("Poucos argumentos. Tente novamente")
+    else:
+        print("Muitos argumentos. Tente novamente")
     exit(1)
 else:
     entrada = sys.argv[1]
     consulta = sys.argv[2]
     saida = sys.argv[3]
+
+    if not (os.path.isfile(entrada) and os.path.isfile(consulta)):
+        print("Arquivo inexistente")
+        exit(1)
+        
 #-----------------------------------------------------------------------------------
 #recebe o arquivo de entrada com todos os registros e organiza-os
 def recebeRegistro():
@@ -35,24 +43,29 @@ def consultaTupla(arquivo, tuplaPrimaria, tuplaSecundaria):
     with open(consulta, 'r') as query, open(saida, 'w') as output:
         condicao = query.readline().strip("\n")
         valor = query.readline()
+        encontrou = 0
+        colunaEscolhida = -1
+        colunas = []
+        linhasSelecionadas = []
 
+        #para o caso de uma condição só
         if '&' not in condicao and '||' not in condicao:
             for i in range(len(tuplaSecundaria[0])):
-                if condicao == tuplaSecundaria[0][i]:
+                if condicao in tuplaSecundaria[0][i]:
                     colunaEscolhida = i
-            
+
             for i in range(len(tuplaSecundaria)):
                 if valor in tuplaSecundaria[i][colunaEscolhida]:
+                    encontrou+= 1
                     for j in range(len(tuplaPrimaria)):
                         if tuplaSecundaria[i][0] == tuplaPrimaria[j][0]:
                             output.write(arquivo[int(tuplaPrimaria[j][1])])
 
+        #para o caso de mais de uma condicao com &
         if '&' in condicao and '||' not in condicao:
             condicao = condicao.split(" & ")
             valor = valor.split(", ")
 
-            colunas = []
-            linhasSelecionadas = []
             #verifica quais colunas está procurando
             for k in range(len(condicao)):
                 for i in range(len(tuplaSecundaria[0])):
@@ -74,13 +87,81 @@ def consultaTupla(arquivo, tuplaPrimaria, tuplaSecundaria):
 
             for i in range(len(linhasSelecionadas)):
                 if linhasSelecionadas[i] != -1:
+                    encontrou+= 1
                     for j in range(len(tuplaPrimaria)):
                         if tuplaSecundaria[linhasSelecionadas[i]][0] == tuplaPrimaria[j][0]:
                             output.write(arquivo[int(tuplaPrimaria[j][1])])
 
-        if '&' in condicao and '||' in condicao:
-            condicao = condicao.split()
+        #para o caso de mais de uma condicao com ||
+        if '&' not in condicao and '||' in condicao:
+            condicao = condicao.split(" || ")
+            valor = valor.split(", ")
 
+            #verifica quais colunas está procurando
+            for k in range(len(condicao)):
+                for i in range(len(tuplaSecundaria[0])):
+                    if condicao[k] in tuplaSecundaria[0][i]:
+                        colunas.append(i)
+            
+            for c in range(len(colunas)):
+                for i in range(len(tuplaSecundaria)):
+                    if valor[c] in tuplaSecundaria[i][colunas[c]]:
+                        encontrou+= 1
+                        for j in range(len(tuplaPrimaria)):
+                            if tuplaSecundaria[i][0] == tuplaPrimaria[j][0]:
+                                output.write(arquivo[int(tuplaPrimaria[j][1])])
+        
+        #para o caso de mais de uma condicao com & e ||
+        if '&' in condicao and '||' in condicao:
+            condicao = condicao.split(" || ")
+            valor = valor.split(", ", len(condicao) -1)
+    
+            #para || é necessário atribuir as duas condições, por isso é splitado no || primeiro
+            for c in range(len(condicao)):
+                if '&' in condicao[c]:
+                    condicao[c] = condicao[c].split(" & ")
+                    valor[c] = valor[c].split(", ")
+                    
+                    #verifica quais colunas está procurando
+                    for k in range(len(condicao[c])):
+                        for i in range(len(tuplaSecundaria[0])):
+                            if condicao[c][k] in tuplaSecundaria[0][i]:
+                                colunas.append(i)
+                    
+                    #verifica quais linhas atendem à primeira condição
+                    for j in range(len(tuplaSecundaria)):
+                        if valor[c][0] in tuplaSecundaria[j][colunas[0]]:
+                            linhasSelecionadas.append(j)
+
+                    #a partir da primeira condição, verifica quais linhas atendem às outras condições
+                    for k in range(1, len(valor[c])):
+                        tamanho = len(linhasSelecionadas)
+                        for i in range(tamanho):
+                            #caso o valor procurado não estiver na linha determinada, invalida essa linha
+                            if (valor[c][k] not in tuplaSecundaria[linhasSelecionadas[i]][colunas[k]]): 
+                                linhasSelecionadas[i] = -1
+
+                    for i in range(len(linhasSelecionadas)):
+                        if linhasSelecionadas[i] != -1:
+                            encontrou+= 1
+                            for j in range(len(tuplaPrimaria)):
+                                if tuplaSecundaria[linhasSelecionadas[i]][0] == tuplaPrimaria[j][0]:
+                                    output.write(arquivo[int(tuplaPrimaria[j][1])])
+                else:
+                    for i in range(len(tuplaSecundaria[0])):
+                        if condicao[c] in tuplaSecundaria[0][i]:
+                            colunaEscolhida = i
+
+                    for i in range(len(tuplaSecundaria)):
+                        if valor[c] in tuplaSecundaria[i][colunaEscolhida]:
+                            encontrou+= 1
+                            for j in range(len(tuplaPrimaria)):
+                                if tuplaSecundaria[i][0] == tuplaPrimaria[j][0]:
+                                    output.write(arquivo[int(tuplaPrimaria[j][1])])
+        
+        #caso nada tenha sido encontrado
+        if encontrou == 0:
+            print("Nenhum resultado foi encontrado")
 #---------------------------------------------------------------------------------------
 def verificaConsulta():
     with open(consulta, 'r') as query:
